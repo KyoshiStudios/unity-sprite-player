f85ff98d35e38567eec3b2245b858229f1bbe6a3
CabbageGod <cabbagegod.dev@gmail.com>
2023-01-17 15:52:52 -0500

    Project Base

---
+++ Assets/Animator/Example/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpritePlayer), typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+    private SpritePlayer player;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = GetComponent<SpritePlayer>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Control attacking
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            player.PlayAnimation("Attack");
+            player.OnCurrentLoopComplete.AddListener(() => {
+                player.PlayAnimation("Idle");
+            });
+        }
+
+        //Control the player moving
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            sprite.flipX = true;
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        } else if(Input.GetKey(KeyCode.RightArrow)) {
+            sprite.flipX = false;
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        } else {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        //Change the animation depending on the speed of the player
+        if(player.GetCurrentAnimation().id != "Attack") { //If the player is currently attacking, don't change the animation
+            if(rb.velocity.magnitude > 0.2f) {
+                if(player.GetCurrentAnimation().id != "Walk") { //Check to make sure you aren't already walking
+                    player.PlayAnimation("Walk");
+                }
+            } else {
+                if(player.GetCurrentAnimation().id != "Idle") { //Check to make sure you aren't already idle
+                    player.PlayAnimation("Idle");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Animator/SpriteAnimation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sprite Animation", menuName = "Sprite Animation")]
+public class SpriteAnimation : ScriptableObject {
+    [Tooltip("The sprites in this animation")]
+    public List<Sprite> sprites = new List<Sprite>();
+    [Tooltip("The time between each frame")]
+    public float fps = 12;
+    [Tooltip("A unique name used to identify this animation")]
+    public string id = "New Animation";
+    [Tooltip("If the animation should loop")]
+    public bool looping = true;
+}
