22815bb812ec4cbc7adb2c391be8469770b905df
CabbageGod <cabbagegod.dev@gmail.com>
2023-03-24 15:45:55 -0400

    Added namespace

--- Assets/KyoshiStudios/Animator/Example/PlayerController.cs
+++ Assets/KyoshiStudios/Animator/Example/PlayerController.cs
@@ -3,77 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using KyoshiStudios.Animation;
 
-[RequireComponent(typeof(SpritePlayer), typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour {
-    private SpritePlayer player;
-    private Rigidbody2D rb;
-    private SpriteRenderer sprite;
+namespace KyoshiStudios.Animation {
 
-    // Start is called before the first frame update
-    void Start() {
-        player = GetComponent<SpritePlayer>();
-        rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+    [RequireComponent(typeof(SpritePlayer), typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour {
+        private SpritePlayer player;
+        private Rigidbody2D rb;
+        private SpriteRenderer sprite;
 
-        SpriteAnimation idle = player.GetAnimation("Idle");
-        SpriteAnimation walk = player.GetAnimation("Walk");
-        SpriteAnimation attack = player.GetAnimation("Attack");
+        // Start is called before the first frame update
+        void Start() {
+            player = GetComponent<SpritePlayer>();
+            rb = GetComponent<Rigidbody2D>();
+            sprite = GetComponent<SpriteRenderer>();
 
-        Transition attackTransition = new Transition(attack, "attack");
-        Transition walkTransition = new Transition(walk, "isWalking");
-        Transition idleTransition = new Transition(idle, "isIdle");
+            SpriteAnimation idle = player.GetAnimation("Idle");
+            SpriteAnimation walk = player.GetAnimation("Walk");
+            SpriteAnimation attack = player.GetAnimation("Attack");
 
-        //might add an option to automate this using all animations
+            Transition attackTransition = new Transition(attack, "attack");
+            Transition walkTransition = new Transition(walk, "isWalking");
+            Transition idleTransition = new Transition(idle, "isIdle");
 
-        //Add them in order of priority
-        idle.AddTransition(attackTransition);
-        idle.AddTransition(walkTransition);
+            //might add an option to automate this using all animations
 
-        walk.AddTransition(attackTransition);
-        walk.AddTransition(idleTransition);
+            //Add them in order of priority
+            idle.AddTransition(attackTransition);
+            idle.AddTransition(walkTransition);
 
-        attack.AddTransition(idleTransition);
-        attack.AddTransition(walkTransition);
-    }
+            walk.AddTransition(attackTransition);
+            walk.AddTransition(idleTransition);
 
-    // Update is called once per frame
-    void Update() {
-        //Control attacking
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            player.SetCondition("attack", true);
-        }
-        if(Input.GetKeyUp(KeyCode.Space)) {
-            player.SetCondition("attack", false);
+            attack.AddTransition(idleTransition);
+            attack.AddTransition(walkTransition);
         }
 
-        bool isWalking = true;
-        //Control the player moving
-        if(Input.GetKey(KeyCode.LeftArrow)) {
-            sprite.flipX = true;
-            rb.velocity = new Vector2(-5, rb.velocity.y);
-        } else if(Input.GetKey(KeyCode.RightArrow)) {
-            sprite.flipX = false;
-            rb.velocity = new Vector2(5, rb.velocity.y);
-        } else {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-            isWalking = false;
-        }
-
-        player.SetCondition("isWalking", isWalking);
-        player.SetCondition("isIdle", !isWalking);
-
-        /*
-        //Change the animation depending on the speed of the player
-        if(player.GetCurrentAnimation().id != "Attack") { //If the player is currently attacking, don't change the animation
-            if(rb.velocity.magnitude > 0.2f) {
-                if(player.GetCurrentAnimation().id != "Walk") { //Check to make sure you aren't already walking
-                    player.PlayAnimation("Walk");
-                }
-            } else {
-                if(player.GetCurrentAnimation().id != "Idle") { //Check to make sure you aren't already idle
-                    player.PlayAnimation("Idle");
-                }
+        // Update is called once per frame
+        void Update() {
+            //Control attacking
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                player.SetCondition("attack", true);
             }
-        }*/
+            if(Input.GetKeyUp(KeyCode.Space)) {
+                player.SetCondition("attack", false);
+            }
+
+            bool isWalking = true;
+            //Control the player moving
+            if(Input.GetKey(KeyCode.LeftArrow)) {
+                sprite.flipX = true;
+                rb.velocity = new Vector2(-5, rb.velocity.y);
+            } else if(Input.GetKey(KeyCode.RightArrow)) {
+                sprite.flipX = false;
+                rb.velocity = new Vector2(5, rb.velocity.y);
+            } else {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+                isWalking = false;
+            }
+
+            player.SetCondition("isWalking", isWalking);
+            player.SetCondition("isIdle", !isWalking);
+
+            /*
+            //Change the animation depending on the speed of the player
+            if(player.GetCurrentAnimation().id != "Attack") { //If the player is currently attacking, don't change the animation
+                if(rb.velocity.magnitude > 0.2f) {
+                    if(player.GetCurrentAnimation().id != "Walk") { //Check to make sure you aren't already walking
+                        player.PlayAnimation("Walk");
+                    }
+                } else {
+                    if(player.GetCurrentAnimation().id != "Idle") { //Check to make sure you aren't already idle
+                        player.PlayAnimation("Idle");
+                    }
+                }
+            }*/
+        }
     }
 }
